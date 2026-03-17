2fbf797032fc26cf9a52489380175b69562014fa
outofband <outofband-ii@proton.me>
2026-03-17 21:31:03 +0100

    feat: Update project documentation and configuration files; enhance data processing and model training modules

--- litigpt/training/trainer.py
+++ litigpt/training/trainer.py
@@ -11,7 +11,7 @@ from transformers import (
     TrainingArguments,
 )
 from peft import LoraConfig, get_peft_model, prepare_model_for_kbit_training
-from trl import SFTTrainer
+from trl.trainer.sft_trainer import SFTTrainer
 from datasets import load_dataset
 import os
 
